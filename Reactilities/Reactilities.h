8434b6015f868db5240a9f23c42e776c6a1839fc
Magnus Eriksson <magnus.eriksson@apegroup.com>
2016-06-24 12:56:57 +0200

    Initial commit

---
+++ Reactilities/Reactilities.h
@@ -0,0 +1,19 @@
+//
+//  Reactilities.h
+//  Reactilities
+//
+//  Created by Magnus Eriksson on 24/06/16.
+//  Copyright © 2016 Magnus Eriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reactilities.
+FOUNDATION_EXPORT double ReactilitiesVersionNumber;
+
+//! Project version string for Reactilities.
+FOUNDATION_EXPORT const unsigned char ReactilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reactilities/PublicHeader.h>
+
+
